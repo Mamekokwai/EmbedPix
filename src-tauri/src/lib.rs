6d74357e643bf5469940905d13348a4a758956c5
XUNCHANG WANG <142494217+Mamekokwai@users.noreply.github.com>
2026-09-19 21:23:50 +0800

    feat: configure tauri updater plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,6 +4,7 @@ mod commands;
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .manage(commands::update::UpdateProgressState::default())
         .invoke_handler(tauri::generate_handler![
             commands::gif::pick_gif_output,
