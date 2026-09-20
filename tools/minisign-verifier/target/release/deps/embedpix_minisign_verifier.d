288746b9ba5d14326c003578ff210eb4b3613ac0
XUNCHANG WANG <142494217+Mamekokwai@users.noreply.github.com>
2026-09-21 03:29:10 +0800

    chore: ignore verifier build output

--- tools/minisign-verifier/target/release/deps/embedpix_minisign_verifier.d
+++
@@ -1,5 +0,0 @@
-E:\Github\EmbedPix\tools\minisign-verifier\target\release\deps\embedpix_minisign_verifier.d: src\main.rs
-
-E:\Github\EmbedPix\tools\minisign-verifier\target\release\deps\embedpix_minisign_verifier.exe: src\main.rs
-
-src\main.rs:
--- tools/minisign-verifier/target/release/embedpix-minisign-verifier.d
+++
@@ -1,1 +0,0 @@
-E:\Github\EmbedPix\tools\minisign-verifier\target\release\embedpix-minisign-verifier.exe: E:\Github\EmbedPix\tools\minisign-verifier\src\main.rs
