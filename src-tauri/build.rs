1dded335838fa4ddf42a5eb2f85621f6bc505fa5
XUNCHANG WANG <142494217+Mamekokwai@users.noreply.github.com>
2026-09-24 08:44:59 +0800

    fix: force windows gui subsystem

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,7 @@
 fn main() {
     tauri_build::build();
+
+    if std::env::var_os("CARGO_CFG_WINDOWS").is_some() {
+        println!("cargo:rustc-link-arg-bin=embedpix=/SUBSYSTEM:WINDOWS");
+    }
 }
